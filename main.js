baef4ce95ef5a4974b7407490fb5749bd3a0cdb2
Jacobondnd <jacobtheleo275@gmail.com>
2025-04-16 21:11:33 -0400

    Conditionally prevent default on touchstart

--- main.js
+++ main.js
@@ -83,7 +83,9 @@ const mouse = new THREE.Vector2();
 // --- Interaction ---
 
 function onPointerDown(event) {
-    event.preventDefault();
+    if (event.type === 'touchstart') {
+        event.preventDefault();
+    }
     // Calculate mouse position in normalized device coordinates (-1 to +1) for both components
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
     mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
